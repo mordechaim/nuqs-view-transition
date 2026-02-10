e4fc64efbffc335d49bc8e33d31aa2452eb20323
Mordechai Meisels <mordechai@codaworks.com>
2026-02-10 12:44:39 -0500

    Reproduction

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,124 @@
-import Image from "next/image";
+'use client';
 
-export default function Home() {
+import { parseAsBoolean, useQueryState } from 'nuqs';
+import { useState, useTransition, ViewTransition } from 'react';
+
+export default function HomePage() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+    <div className='grid grid-flow-col grid-rows-[40px_1fr] auto-cols-fr gap-5 m-10'>
+      <span>Native state</span>
+      <NativeStateTransition />
+
+      <span>Query state</span>
+      <QueryStateTransition />
     </div>
+  );
+}
+
+function NativeStateTransition() {
+  const [details, setDetails] = useState(false);
+  const [pending, startTransition] = useTransition();
+
+  const handleToggle = () => {
+    startTransition(() => {
+      setDetails((prev) => !prev);
+    });
+  };
+
+  return (
+    <section className=' border border-gray-200 bg-gray-50 flex flex-col gap-6 p-5'>
+      <div className='flex justify-between'>
+        {details && (
+          <ViewTransition name='native-details'>
+            <span>Details</span>
+          </ViewTransition>
+        )}
+        {!details && (
+          <ViewTransition name='native-home'>
+            <span>Home</span>
+          </ViewTransition>
+        )}
+
+        <button
+          className='border border-gray-400 px-2 py-0.5 bg-white'
+          onClick={handleToggle}
+        >
+          Go to{' '}
+          {details && (
+            <ViewTransition name='native-home'>
+              <span>Home</span>
+            </ViewTransition>
+          )}
+          {!details && (
+            <ViewTransition name='native-details'>
+              <span>Details</span>
+            </ViewTransition>
+          )}
+        </button>
+      </div>
+    </section>
+  );
+}
+
+function QueryStateTransition() {
+  const [details, setDetails] = useQueryState(
+    'details',
+    parseAsBoolean.withDefault(false),
+  );
+  const [sync, setSync] = useState(true);
+  const [pending, startTransition] = useTransition();
+
+  const handleToggle = async () => {
+    if (sync) {
+      startTransition(() => {
+        setDetails((prev) => !prev);
+      });
+    } else {
+      startTransition(async () => {
+        await setDetails((prev) => !prev);
+      });
+    }
+  };
+
+  return (
+    <section className=' border border-gray-200 bg-gray-50 flex flex-col gap-6 p-5'>
+      <div className='flex justify-between'>
+        {details && (
+          <ViewTransition name='query-details'>
+            <span>Details</span>
+          </ViewTransition>
+        )}
+        {!details && (
+          <ViewTransition name='query-home'>
+            <span>Home</span>
+          </ViewTransition>
+        )}
+
+        <button
+          className='border border-gray-400 px-2 py-0.5 bg-white'
+          onClick={handleToggle}
+        >
+          Go to{' '}
+          {details && (
+            <ViewTransition name='query-home'>
+              <span>Home</span>
+            </ViewTransition>
+          )}
+          {!details && (
+            <ViewTransition name='query-details'>
+              <span>Details</span>
+            </ViewTransition>
+          )}
+        </button>
+      </div>
+      <label className='flex gap-3 items-center'>
+        <input
+          type='checkbox'
+          checked={sync}
+          onChange={(e) => setSync(e.target.checked)}
+        />
+        Sync transition
+      </label>
+    </section>
   );
 }
